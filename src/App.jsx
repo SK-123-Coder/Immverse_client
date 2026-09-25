b95f658db7a41ceb2b1c8c86c1f00573478ad6a0
SK_Coder <sarangkhandateofficial@gmail.com>
2026-09-25 16:18:16 +0530

    0.2

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/upload", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/upload`, {
         method: "POST",
         body: formData,
       });
